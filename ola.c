4225bd18c33ef44f672b5f51eeaa4dc10996ee12
jessicapcosta <73697991+jessicapcosta@users.noreply.github.com>
2020-10-30 11:56:39 +0000

    Add files via upload

---
+++ ola.c
@@ -0,0 +1,16 @@
+/**
+ * @file aula.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-10-26
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#include<stdio.h>
+int main()
+{
+    int x=1;
+    printf("%d\n", x);
+}
